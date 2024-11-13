9d924866c9ba1aa8439588b38bbdbf3f4ee3baad
unknown <macarrion44@utpl.edu.ec>
2024-11-13 11:44:44 -0500

    cambios

--- ejemplo-condicionales-anidados/Condicionales/src/paquete2/Comercio.java
+++ ejemplo-condicionales-anidados/Condicionales/src/paquete2/Comercio.java
@@ -69,7 +69,7 @@ public class Comercio {
         }
         
        
-        adicionalSeguro = (sueldoBasico * 8)/100;
+        adicionalSeguro = (sueldoBasico * porcentajeSeguro)/100;
         sueldoFinal = adicionalSeguro + bono + sueldoBasico;
         
         System.out.printf("Reporte de Empleado EL GRAN COMERCIO\n\n"
